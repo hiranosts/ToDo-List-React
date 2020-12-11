a68c53312c68b13553a3c7f9796c46896b7bb2c3
hiranosts <sat.1126.hirano@gmail.com>
2020-12-11 14:07:02 +0900

    make file

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css';
+
+function App(){
+  return(
+    <div className="text-center">
+      <h1 className="main-title">ToDoリスト React</h1>
+      <input type="textbox"></input>
+      <button>追加</button>
+    </div>
+  );
+}
+
+
+
+export default App
